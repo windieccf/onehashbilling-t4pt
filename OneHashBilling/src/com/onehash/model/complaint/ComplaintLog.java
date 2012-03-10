0cf8e1cd4aae462a52d3fe7bbc2e8ba2b5d44eed
windieccf <windieccf@gmail.com>
2012-03-10 15:45:52 +0000

    add date utils and complaintlog test case

--- OneHashBilling/src/com/onehash/model/complaint/ComplaintLog.java
+++ OneHashBilling/src/com/onehash/model/complaint/ComplaintLog.java
@@ -19,37 +19,158 @@
  */
 package com.onehash.model.complaint;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 import com.onehash.enumeration.EnumComplaint;
 import com.onehash.model.base.BaseEntity;
+import com.onehash.utility.OneHashDateUtil;
+import com.onehash.utility.OneHashStringUtil;
 
 @SuppressWarnings("serial")
 public class ComplaintLog extends BaseEntity{
+	
+	public static final String COMPLAINT_PREFIX = "COMP";
 
-	private String issueNo;
-	public String getIssueNo() {return issueNo;}
-	public void setIssueNo(String issueNo) {this.issueNo = issueNo;}
-	
+	private String issueNo; // format as COMP201203100001
 	private String issueDescription;
-	public String getIssueDescription() {return issueDescription;}
-	public void setIssueDescription(String issueDescription) {this.issueDescription = issueDescription;}
-	
 	private Date complaintDate;
-	public Date getComplaintDate() {return complaintDate;}
-	public void setComplaintDate(Date complaintDate) {this.complaintDate = complaintDate;}
-	
 	private Date closedDate;
-	public Date getClosedDate() {return closedDate;}
-	public void setClosedDate(Date closedDate) {this.closedDate = closedDate;}
-	
 	private EnumComplaint status;
-	public EnumComplaint getStatus() {return status;}
-	public void setStatus(String status) {this.status = EnumComplaint.getEnumByStatus(status);}
-	
 	private String followUp;
-	public String getFollowUp() {return followUp;}
-	public void setFollowUp(String followUp) {this.followUp = followUp;}
 	
+	/**
+	 * issueNo construct as COMPLAINT_PREFIX + complaintDateNo
+	 * @param issueDescription
+	 * @param complaintDate
+	 * @param allIssueNoList
+	 */
+	public ComplaintLog(String issueDescription, Date complaintDate, ArrayList<String> allIssueNoList) {
+		String complaintDateNo = null;
+		String strDate = OneHashDateUtil.format(OneHashDateUtil.getDate(),
+				"yyyyMMdd");
+		long todayIssueNo = this.getLastComplaintNoUsedToday(strDate, allIssueNoList);
+
+		complaintDateNo = getComplaintDateNo(complaintDateNo, strDate,
+				todayIssueNo);
+
+		this.setIssueNo(COMPLAINT_PREFIX + complaintDateNo);
+		this.setComplaintDate(complaintDate);
+		this.setIssueDescription(issueDescription);
+	}
+	
+	public ComplaintLog () {
+		//default constructor
+	}
+
+	public String getIssueNo() {
+		return issueNo;
+	}
+
+	public void setIssueNo(String issueNo) {
+		this.issueNo = issueNo;
+	}
+
+	public String getIssueDescription() {
+		return issueDescription;
+	}
+
+	public void setIssueDescription(String issueDescription) {
+		this.issueDescription = issueDescription;
+	}
+
+	public Date getComplaintDate() {
+		return complaintDate;
+	}
+
+	public void setComplaintDate(Date complaintDate) {
+		this.complaintDate = complaintDate;
+	}
+
+	public Date getClosedDate() {
+		return closedDate;
+	}
+
+	public void setClosedDate(Date closedDate) {
+		this.closedDate = closedDate;
+	}
+
+	public EnumComplaint getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = EnumComplaint.getEnumByStatus(status);
+	}
+
+	public String getFollowUp() {
+		return followUp;
+	}
+
+	public void setFollowUp(String followUp) {
+		this.followUp = followUp;
+	}
+	
+	/**
+	 * Retrieving today max issue number
+	 * @param strDate
+	 * @param AllIssueNoList
+	 * @return current maximum issue number (last 4 digits)
+	 */
+	private long getLastComplaintNoUsedToday(String strDate, ArrayList<String> AllIssueNoList) {
+		ArrayList<String> issueNoMaxList = new ArrayList<String>();
+		if (AllIssueNoList != null && AllIssueNoList.size() > 0) {
+			for (String issueNo : AllIssueNoList) {
+				int issueNoIndex = issueNo.indexOf(strDate);
+				if (issueNoIndex > 0) {
+					issueNoMaxList.add(issueNo);
+				}
+			}
+		}
+
+		if (issueNoMaxList.size() == 0) {
+			return -1;
+		}
+
+		long max = 0;
+
+		for (String issueNoMax : issueNoMaxList) {
+			String tempNum = issueNoMax.substring(12);
+			if (max < Long.parseLong(tempNum)) {
+				max = Long.parseLong(tempNum);
+			}
+		}
+		return max;
+	}
+
+	/**
+	 * Retrieving complaint Date Number
+	 * @param complaintDateNo
+	 * @param strDate
+	 * @param todayIssueNo
+	 * @return complaintDateNo, format as '201203100001'
+	 */
+	private String getComplaintDateNo(String complaintDateNo, String strDate,
+			long todayIssueNo) {
+		String[] strDateArr = null;
+		if (todayIssueNo < 0) {
+			strDateArr = OneHashDateUtil.parseDateToStr(OneHashDateUtil
+					.getDate());
+
+			if (strDateArr != null) {
+				String sbDay = strDateArr[0];
+				String sbMonth = strDateArr[1];
+				String sbYear = strDateArr[2];
+
+				complaintDateNo = OneHashStringUtil.rPad(strDate, 12, "0");
+			}
+		} else {
+			long lTempNo = todayIssueNo + 1;
+			String newTempNo = String.valueOf(lTempNo);
+			newTempNo = OneHashStringUtil.lPad(newTempNo, 4, "0");
+			complaintDateNo = strDate + newTempNo;
+		}
+		return complaintDateNo;
+	}
 	
 }
--- OneHashBilling/src/com/onehash/utility/OneHashStringUtil.java
+++ OneHashBilling/src/com/onehash/utility/OneHashStringUtil.java
@@ -11,7 +11,7 @@
  * -----------------------------------------------------------------
  * DATE             AUTHOR          REVISION		DESCRIPTION
  * 10 March 2012    Robin Foe	    0.1				Class creating
- * 													
+ * 10 March 2012	Chen Changfeng	0.2				Adding new Util methods											
  * 													
  * 													
  * 													
@@ -38,5 +38,122 @@ public class OneHashStringUtil {
 	public static String getEmptyIfNull(String text){
 		return OneHashStringUtil.isEmpty(text) ? "" : text;
 	}
+	
+	/**
+     * To pad the given string with spaces up to the given length. <br>
+     * e.g. rPad("ABCD", 10, ' ') returns "ABCD      " which has a length of 10.
+     *
+     * This method has built-in 'intelligence' to handle cases where
+     * calling method tries to be funny and supply the following<br>
+     * - rPad("abc", 10, "123") it will return, "abc1231231"
+     *
+     * @param str String to be padded
+     * @param length The required length of the resulted string
+     * @param padString The required padding string.
+     * @return The padded string.
+     * If str already <I>longer</I> than <I>length</I>, return str itself.
+     */
+    public static String rPad ( String str, int length, String padString ) {
+        int lOriginal = str.length();
+        int lPadStr = padString.length();
+        int times2Pad = 0;
+        int lPadded = 0;
+
+        if ( lOriginal >= length )
+            return str;
+
+        StringBuffer sb = new StringBuffer( str ); //add the original str first
+        String padded;
+
+        times2Pad = ( length - lOriginal ) / lPadStr;  //will give (1) if 3/2
+
+        padded = duplicate( padString, times2Pad );
+        lPadded = padded.length();
+        sb.append( padded );        //pad in the repetitive characters
+
+        //if still insufficient by the modulus e.g. 30/20 is 10
+        if ( lOriginal + lPadded < length ) {
+            int more = length - ( lOriginal + lPadded );
+
+            //add in the difference which is less entire length of padStr
+            sb.append( padString.substring( 0, more ) );
+        }
+
+        return sb.toString();
+    }
+    
+    /**
+     * To return a string which is filled with a specified string.
+     * e.g. duplicate("*", 5) returns "*****", duplicate("OK", 3) returns "OKOKOK"
+     * repeated for given number of times
+     *
+     * @param str String to be repeated/duplicated
+     * @param times Number of time the string to be repeated/duplicated
+     * @return The resulted string with <code>str</code> repeated the specified number of times.
+     */
+    public static String duplicate ( String str, int times ) {
+        StringBuffer result = new StringBuffer();
+
+        for ( int i = 0; i < times; i++ ) {
+            result.append( str );
+        }
+        return ( result.toString() );
+    }
+    
+    /**
+     * To pad the given string with a user specified character on the left up to the
+     * given length.
+     * e.g. lPad("ABCD", 10, 'X') returns "XXXXXXABCD" which has a length of 10.
+     * This method has built-in 'intelligence' to handle cases where calling method
+     * If <I>str</I> already longer than <I>length</I>, return <I>str</I> itself.
+     * tries to be funny and supply the following :
+     * - lPad("abc", 10, "123") it will return, "1231231abc"
+     *
+     * @param str String to be padded
+     * @param length he required length of the resulted string.
+     * @param padString The required padding string
+     * @return The padded string
+     */
+    public static String lPad ( String str, int length, String padString ) {
+        int lOriginal = str.length();
+        int lPadStr = padString.length();
+        int times2Pad = 0;
+        int lPadded = 0;
+
+        if ( lOriginal >= length )
+            return str;
+
+        StringBuffer sb = new StringBuffer();
+        String padded;
+
+        times2Pad = ( length - lOriginal ) / lPadStr;  //will give (1) if 3/2
+
+        padded = duplicate( padString, times2Pad );
+        lPadded = padded.length();
+        sb.append( padded );        //pad in the repetitive characters
+
+        //if still insufficient by the modulus e.g. 30/20 is 10
+        if ( lOriginal + lPadded < length ) {
+            int more = length - ( lOriginal + lPadded );
+
+            //add in the difference which is less entire length of padStr
+            sb.append( padString.substring( 0, more ) );
+        }
+
+        sb.append( str ); //pad the original string behind
+
+        return sb.toString();
+    }
+    
+    /**
+     * Pads the string with prevailing spaces.
+     *
+     * @param str String to be padded with spaces on the left.
+     * @param len The number of spaces to pad to the left of the string.
+     * @return The space-padded string.
+     */
+    public static String lPad ( String str, int len ) {
+        return lPad( str, len, " " );
+    }
 
 }
