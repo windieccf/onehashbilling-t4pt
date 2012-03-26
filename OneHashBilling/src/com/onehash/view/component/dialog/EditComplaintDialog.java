815b6b5a506ba30ab4c177e2e937acb5ebc00aba
windieccf <windieccf@gmail.com>
2012-03-26 08:15:05 +0000

    interim commit

--- OneHashBilling/src/com/onehash/view/component/dialog/EditComplaintDialog.java
+++ OneHashBilling/src/com/onehash/view/component/dialog/EditComplaintDialog.java
@@ -1,59 +1,79 @@
+/*
+ * CONFIDENTIAL AND PROPRIETARY SOURCE CODE OF
+ * Institute of Systems Science, National University of Singapore
+ *
+ * Copyright 2012 Team 4(Part-Time), ISS, NUS, Singapore. All rights reserved.
+ * Use of this source code is subjected to the terms of the applicable license
+ * agreement.
+ *
+ * -----------------------------------------------------------------
+ * REVISION HISTORY
+ * -----------------------------------------------------------------
+ * DATE             AUTHOR          REVISION		DESCRIPTION
+ * 26 March 2012    Chen Changfeng	0.1				Class creating 													
+ * 													
+ * 													
+ * 													
+ * 
+ */
 package com.onehash.view.component.dialog;
 
 
 import java.awt.GridLayout;
-import java.util.ArrayList;
 
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
-import javax.swing.JTextField;
 
 import com.onehash.controller.OneHashDataCache;
 import com.onehash.model.complaint.ComplaintLog;
-import com.onehash.model.customer.Customer;
 import com.onehash.view.component.dialogUtil.OkCancelDialog;
-import com.onehash.view.panel.customer.CustomerEditPanel;
+import com.onehash.view.panel.complaint.ComplaintEditPanel;
 
 @SuppressWarnings("serial")
 public class EditComplaintDialog extends OkCancelDialog {
 
 	private OneHashDataCache dataCache;
-	private CustomerEditPanel customerEdit;
 	private ComplaintLog complaintLog;
     private JTextArea followUpField;
+    private JComboBox comboBox;
 
-    public EditComplaintDialog (CustomerEditPanel customerEdit, ComplaintLog complaintLog) {
-        super (customerEdit.getMainFrame(), "Edit Complaint");
-        dataCache=OneHashDataCache.getInstance();
-        this.customerEdit = customerEdit;
-        this.complaintLog = complaintLog;
-    }
+	public EditComplaintDialog(ComplaintEditPanel complaintEdit,
+			ComplaintLog complaintLog) {
+		super(complaintEdit.getMainFrame(), "Edit Complaint", complaintLog
+				.getIssueDescription(), complaintLog.getStatus().getLabel());
+		dataCache = OneHashDataCache.getInstance();
+		this.complaintLog = complaintLog;
+	}
 
-    protected JPanel createFormPanel () {
-        JPanel p = new JPanel ();
-        p.setLayout (new GridLayout (0, 2));
-        p.add (new JLabel ("Description"));
+	protected JPanel createFormPanel(String issueDes, String status) {
+		JPanel p = new JPanel();
+		p.setLayout(new GridLayout(0, 2));
+		p.add(new JLabel("Description"));
 
-        p.add (new JLabel ("Description text"));
-        
-        p.add (new JLabel ("Follow up"));
-        followUpField = new JTextArea(10, 15);
-        p.add (followUpField);
-        p.add (new JLabel ("Status"));
-//        secondField = new JTextField (20);
-//        p.add (secondField);
-        return p;
-    }
+		p.add(new JLabel(issueDes));
+
+		p.add(new JLabel("Follow up"));
+		followUpField = new JTextArea(10, 15);
+		p.add(followUpField);
+		p.add(new JLabel("Status"));
+		comboBox = new JComboBox();
+		comboBox.addItem("Active");
+		comboBox.addItem("Closed");
+		comboBox.addItem("Pending");
+		comboBox.addItem("Follow up");
+		comboBox.setSelectedItem(status);
+		p.add(comboBox);
+		return p;
+	}
 
     protected boolean performOkAction () {
-        String status = followUpField.getText();
+        String status = (String) comboBox.getSelectedItem();
+        System.out.println(status);
         String followup = followUpField.getText();
         try {
 			dataCache.updateComplaintLog(complaintLog, status, followup);
-			System.out.println(followUpField.getText());
-			Customer cus = (Customer)customerEdit.getSelectedEntity();
-			System.out.println("2"+cus.getName());
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- OneHashBilling/src/com/onehash/view/panel/complaint/ComplaintEditPanel.java
+++ OneHashBilling/src/com/onehash/view/panel/complaint/ComplaintEditPanel.java
@@ -1,3 +1,21 @@
+/*
+ * CONFIDENTIAL AND PROPRIETARY SOURCE CODE OF
+ * Institute of Systems Science, National University of Singapore
+ *
+ * Copyright 2012 Team 4(Part-Time), ISS, NUS, Singapore. All rights reserved.
+ * Use of this source code is subjected to the terms of the applicable license
+ * agreement.
+ *
+ * -----------------------------------------------------------------
+ * REVISION HISTORY
+ * -----------------------------------------------------------------
+ * DATE             AUTHOR          REVISION		DESCRIPTION
+ * 26 March 2012    Chen Changfeng	0.1				Class creating 													
+ * 													
+ * 													
+ * 													
+ * 
+ */
 package com.onehash.view.panel.complaint;
 
 import java.awt.Dimension;
@@ -5,17 +23,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
 
+import com.onehash.controller.OneHashDataCache;
 import com.onehash.model.base.BaseEntity;
 import com.onehash.model.complaint.ComplaintLog;
 import com.onehash.model.customer.Customer;
 import com.onehash.model.scalar.ButtonAttributeScalar;
+import com.onehash.utility.OneHashDateUtil;
 import com.onehash.view.OneHashGui;
 import com.onehash.view.component.FactoryComponent;
 import com.onehash.view.component.dialog.AddComplaintDialog;
+import com.onehash.view.component.dialog.EditComplaintDialog;
 import com.onehash.view.component.listener.ButtonActionListener;
 import com.onehash.view.component.listener.MouseTableListener;
 import com.onehash.view.component.tablemodel.OneHashTableModel;
@@ -42,6 +64,10 @@ public class ComplaintEditPanel extends BasePanel implements BaseOperationImpl ,
 	@Override
 	protected void init() {
 		this.customer  = new Customer();
+		this.registerComplaintListComponent();
+	}
+	
+	protected void registerComplaintListComponent() {
 		// Add ComplaintListPanel
 		JTable complaintTable = new JTable();
 		complaintTable.setPreferredScrollableViewportSize(new Dimension(100, 70));
@@ -51,13 +77,13 @@ public class ComplaintEditPanel extends BasePanel implements BaseOperationImpl ,
 		complaintTable.addMouseListener(new MouseTableListener(this,"loadEditScreen"));
         
         JScrollPane complaintScrollPane = new JScrollPane(complaintTable);
-        complaintScrollPane.setBounds(20,20,400,115);
+        complaintScrollPane.setBounds(20,20,700,315);
 		super.registerComponent(COMPLAINT_TABLE, complaintScrollPane);
 		
-		JButton complaintaddButton = FactoryComponent.createButton("Add", new ButtonAttributeScalar(20, 150, 100, 23 , new ButtonActionListener(this,"addComplaintLog")));
+		JButton complaintaddButton = FactoryComponent.createButton("Add", new ButtonAttributeScalar(20, 350, 100, 23 , new ButtonActionListener(this,"addComplaintLog")));
 		super.registerComponent(COMPLAINT_BUTTON_ADD , complaintaddButton);
 		
-		JButton complaintremoveButton = FactoryComponent.createButton("Remove", new ButtonAttributeScalar(140, 150, 100, 23 , new ButtonActionListener(this,"removeComplaintLog")));
+		JButton complaintremoveButton = FactoryComponent.createButton("Remove", new ButtonAttributeScalar(140, 350, 100, 23 , new ButtonActionListener(this,"removeComplaintLog")));
 		super.registerComponent(COMPLAINT_BUTTON_REM , complaintremoveButton);
 	}
 	
@@ -73,7 +99,19 @@ public class ComplaintEditPanel extends BasePanel implements BaseOperationImpl ,
 	@Override
 	public void initializeCustomer(Customer customer) {
 		this.customer = customer;
-		// TODO :: perform init
+		this.refreshComplaintListComponent();
+		
+	}
+
+	private void refreshComplaintListComponent() {
+		JTable complaintTable = new JTable();
+		complaintTable.setPreferredScrollableViewportSize(new Dimension(100, 70));
+		complaintTable.setFillsViewportHeight(true);
+		complaintTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		complaintTable.setModel(new OneHashTableModel(this.getComplaintTableColumnNames() , this.getComplaintData(this.customer)));
+		complaintTable.addMouseListener(new MouseTableListener(this,"loadEditScreen"));
+		JScrollPane servicePlansScrollPane = (JScrollPane) super.getComponent(COMPLAINT_TABLE);
+		servicePlansScrollPane.setViewportView(complaintTable);
 		
 	}
 
@@ -83,7 +121,7 @@ public class ComplaintEditPanel extends BasePanel implements BaseOperationImpl ,
 	
 	
 	public String[] getComplaintTableColumnNames(){
-		return new String[]{"Issue No.", "Complaint Date" , "Closed Date", "Status"};
+		return new String[]{"Issue No.", "Description", "Complaint Date" , "Closed Date", "Status"};
 	}
 	
 	public Object[][] getComplaintData(Customer customer){
@@ -93,19 +131,20 @@ public class ComplaintEditPanel extends BasePanel implements BaseOperationImpl ,
 			System.out.println(c.getIssueNo());
 			
 		}
-		Object[][] rowData = new String[1][4];
+		Object[][] rowData = new String[1][5];
 		if (complaintLogs == null)
 			complaintLogs = new ArrayList<ComplaintLog>();
 		if(complaintLogs.isEmpty()){
-			rowData = new Object[0][4];
+			rowData = new Object[0][5];
 		}else{
-			rowData = new Object[complaintLogs.size()][4];
+			rowData = new Object[complaintLogs.size()][5];
 			for(int i = 0 ; i < complaintLogs.size(); i++){
 				ComplaintLog complaintLog = complaintLogs.get(i);
 				rowData[i][0] = complaintLog.getIssueNo();
-				rowData[i][1] = complaintLog.getComplaintDate();
-				rowData[i][2] = complaintLog.getClosedDate();
-				rowData[i][3] = complaintLog.getStatus();
+				rowData[i][1] = complaintLog.getIssueDescription();
+				rowData[i][2] = OneHashDateUtil.format(complaintLog.getComplaintDate(), "dd/MM/yyyy");
+				rowData[i][3] = OneHashDateUtil.format(complaintLog.getClosedDate(), "dd/MM/yyyy");
+				rowData[i][4] = complaintLog.getStatus().getLabel();
 			}
 		}
 		return rowData;
@@ -115,6 +154,32 @@ public class ComplaintEditPanel extends BasePanel implements BaseOperationImpl ,
 		AddComplaintDialog complaintDialog = new AddComplaintDialog(this);
 		complaintDialog.pack();
 		complaintDialog.setVisible (true);
+		//TODO immediately refresh
+		this.refreshComplaintListComponent();
+	}
+	
+	public void removeComplaintLog() {
+		JScrollPane selectedComplaintLogScrollPane = (JScrollPane)super.getComponent(COMPLAINT_TABLE);
+		JTable ComplaintLogJTable = (JTable)selectedComplaintLogScrollPane.getViewport().getView();
+		
+		complaintLogs.remove(ComplaintLogJTable.getSelectedRow());
+		JOptionPane.showMessageDialog(this, "Complaint Successfully Removed");
+		this.refreshComplaintListComponent();
+
+	}
+	
+	public void loadEditScreen(String parameter) throws Exception {
+		JScrollPane selectedComplaintLogScrollPane = (JScrollPane)super.getComponent(COMPLAINT_TABLE);
+		JTable ComplaintLogJTable = (JTable)selectedComplaintLogScrollPane.getViewport().getView();
+		String issueNumber = (String) ComplaintLogJTable.getValueAt(ComplaintLogJTable.getSelectedRow(), 0);
+		System.out.println(issueNumber);
+		ComplaintLog complaintLog = OneHashDataCache.getInstance().getComplaintLogByIssueNo(issueNumber, (ArrayList<ComplaintLog>) complaintLogs);
+		EditComplaintDialog editCompalintDialog = new EditComplaintDialog(this, complaintLog);
+		System.out.println(complaintLog.getIssueDescription());
+		
+		editCompalintDialog.pack();
+		editCompalintDialog.setVisible (true);
+
 	}
 
 
